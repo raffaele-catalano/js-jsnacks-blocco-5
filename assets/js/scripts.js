b59b9ca1163c967f04a86c160a22b3d22878ad09
raffaele-catalano <raf.catalano@gmail.com>
2023-04-04 15:51:42 +0200

    people objects array setup

---
+++ assets/js/scripts.js
@@ -0,0 +1,37 @@
+const container = document.querySelector('.container');
+const container2 = document.querySelector('.container-filter');
+
+const actors = [
+    {
+        firstName: 'Tom',
+        lastName: 'Holland',
+        age: 27
+    },
+    {
+        firstName: 'Jennifer',
+        lastName: 'Aniston',
+        age: 54
+    },
+    {
+        firstName: 'Millie Bobby',
+        lastName: 'Brown',
+        age: 17
+    },
+    {
+        firstName: 'Richard',
+        lastName: 'Gere',
+        age: 72
+    },
+    {
+        firstName: 'Helen',
+        lastName: 'Mirren',
+        age: 78
+    },
+    {
+        firstName: 'Noah',
+        lastName: 'Jupe',
+        age: 15
+    }
+]
+
+//////////////////////////////////////////////////////////////
