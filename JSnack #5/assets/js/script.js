6dd6501ba1dd0fba89e1836a99d0b76d86742785
raffaele-catalano <raf.catalano@gmail.com>
2023-04-06 16:17:34 +0200

    set-up bikes array

---
+++ JSnack #5/assets/js/script.js
@@ -0,0 +1,48 @@
+const container         = document.querySelector('.container');
+const containerLight    = document.querySelector('.container_light');
+
+const bikeShop          = [
+    {
+        model:  'Gravel',
+        name:   'Crosshill 3.0',
+        weight:  1800,
+        price:   12,
+        image:  'gravel.jpg',
+    },
+    {
+        model:  'Electric',
+        name:   'e-Xelius 600',
+        weight:  14,
+        price:   5000,
+        image:  'electric.jpg',
+    },
+    {
+        model:  'Mountain',
+        name:   'Overvolt HT',
+        weight:  24,
+        price:   3000,
+        image:  'overvolt.jpg',
+    },
+    {
+        model:  'Trekking',
+        name:   'TreK 3.0',
+        weight:  760,
+        price:   16,
+        image:  'trekking.jpg',
+    },
+    {
+        model:  'Road',
+        name:   'Xelius SL',
+        weight:  3,
+        price:   11000,
+        image:  'road.jpg',
+    },
+    {
+        model:  'Fixed',
+        name:   'Aerostorm DRS',
+        weight:  5,
+        price:   8000,
+        image:  'fixed.jpg',
+    },
+]
+//////////////////////////////////////////////////////////////////////////////
