14a8bab609ca1d455fee1a33a70fc188bd76a82d
raffaele-catalano <raf.catalano@gmail.com>
2023-04-06 15:23:57 +0200

    minor fix

--- JSnack #4/assets/js/script.js
+++ JSnack #4/assets/js/script.js
@@ -4,10 +4,7 @@ dei loro voti di esame...
 
 1. Per preparare l’aula di un nuovo corso, dobbiamo stampare le targhe col nome degli studenti:
 creare una lista contenente il loro nome tutto in maiuscolo
-ES (Marco della Rovere => MARCO DELLA ROVERE);
-
 2. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70
-
 3. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70 e id
 superiore a 120.
 
@@ -21,6 +18,7 @@ Id      Name                              Grades
 102     Piero della Francesca               50
 120     Francesca da Polenta                84
 */
+
 const container         = document.querySelector('.container');
 const containerOver70   = document.querySelector('.container_over70');
 const containerOver120  = document.querySelector('.container_over120');
